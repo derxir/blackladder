d829ed20443ee03b0023785d3fab2361276715b4
Justin <jiawei.yue1991@me.com>
2017-07-30 00:31:48 +0100

    env setup

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,7 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.network "private_network", ip: "10.0.0.10"
-  config.vm.provision :shell, :path => "provision-ubuntu-14.04.sh"
+  config.vm.provision :shell, :path => "install.sh"
 
   config.vm.provider "virtualbox" do |vb|
     vb.customize ["modifyvm", :id, "--cpuexecutioncap", "50", "--cpus", "1"]
---
+++ install.sh
@@ -0,0 +1,36 @@
+sudo su 
+apt-get update
+
+DBNAME=ttladder
+DBUSER=vagrant
+DBPASSWD=vagrant
+
+# git nginx
+apt-get install -y git vim nginx >> /vagrant/vm_build.log 2>&1
+
+# Nginx
+rm -rf /usr/share/nginx/html
+ln -fs /vagrant /usr/share/nginx/html
+
+# memcached
+# apt-get install memcached
+
+# flask
+apt-get -y install python3-pip
+sudo apt-get install python-pip python-dev libmysqlclient-dev MySQL-python >> /vagrant/vm_build.log 2>&1
+sudo pip3 install flask 
+
+export FLASK_APP=/vagrant/app.py
+export FLASK_DEBUG=1
+# cd /vagrant
+# python3 -m flask run --host=0.0.0.0
+
+# mysql
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASSWD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASSWD"
+
+apt-get -y install mysql-server >> /vagrant/vm_build.log 2>&1
+
+echo -e "\n--- Setting up our MySQL user and db ---\n"
+sudo mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME" >> /vagrant/vm_build.log 2>&1
+sudo mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASSWD'" >> /vagrant/vm_build.log 2>&1
