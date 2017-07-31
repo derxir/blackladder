af878c87be9dae19ea01e86f11e3490413485e55
Justin <jiawei.yue1991@me.com>
2017-07-31 23:07:30 +0100

    changed dbname to be inline with project name

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo su 
 apt-get update
 
-DBNAME=ttladder
+DBNAME=blackladder
 DBUSER=vagrant
 DBPASSWD=vagrant
 
@@ -15,16 +15,6 @@ ln -fs /vagrant /usr/share/nginx/html
 # memcached
 # apt-get install memcached
 
-# flask
-apt-get -y install python3-pip
-sudo apt-get install python-pip python-dev libmysqlclient-dev MySQL-python >> /vagrant/vm_build.log 2>&1
-sudo pip3 install flask 
-
-export FLASK_APP=/vagrant/app.py
-export FLASK_DEBUG=1
-# cd /vagrant
-# python3 -m flask run --host=0.0.0.0
-
 # mysql
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASSWD"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASSWD"
@@ -34,3 +24,14 @@ apt-get -y install mysql-server >> /vagrant/vm_build.log 2>&1
 echo -e "\n--- Setting up our MySQL user and db ---\n"
 sudo mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME" >> /vagrant/vm_build.log 2>&1
 sudo mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASSWD'" >> /vagrant/vm_build.log 2>&1
+
+# flask
+apt-get -y install python3-pip
+sudo apt-get install python-pip python-dev libmysqlclient-dev MySQL-python >> /vagrant/vm_build.log 2>&1
+sudo pip3 install flask 
+
+export FLASK_APP=/vagrant/app.py
+export FLASK_DEBUG=1
+cd /vagrant
+pip3 install -r requirements.txt
+# python3 -m flask run --host=0.0.0.0
